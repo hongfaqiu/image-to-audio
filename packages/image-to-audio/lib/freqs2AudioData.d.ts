3d3ae600d4af848dcbeb8a466422c00abeb81ccf
hongfaqiu <951142905@qq.com>
2023-02-06 11:27:20 +0800

    chore: remove lib

--- packages/image-to-audio/lib/freqs2AudioData.d.ts
+++
@@ -1,5 +0,0 @@
-export type Freqs2AudioOptions = {
-    sampleRate: number;
-    seconds: number;
-};
-export declare function freqs2AudioData(freqs: number[], options: Freqs2AudioOptions): Float32Array;
--- packages/image-to-audio/lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { imageToAudio } from "./imageToAudio";
-export * from "./AudioEncoder";
-export * from './decodeImage';
-export * from './imageToAudio';
-export * from './encodeImage2Freqs';
-export * from './freqs2AudioData';
-export default imageToAudio;
