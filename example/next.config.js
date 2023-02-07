bf2236a24d4485cb50b07219823b3dbad6c5db34
hongfaqiu <951142905@qq.com>
2023-02-07 15:12:42 +0800

    build: export example to static page

--- example/next.config.js
+++ example/next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: false,
   transpilePackages: ['@douyinfe/semi-ui', '@douyinfe/semi-icons'],
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    }
+  },
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
